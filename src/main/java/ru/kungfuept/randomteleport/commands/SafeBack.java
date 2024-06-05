25fa33c8111587deaf2bd98a8668170e861cc821
KungfuEpt <saet.ayaz@yandex.ru>
2024-06-05 15:22:03 +0300

    Added cooldown. Check if sender is console. Check if player teleported into block.

--- src/main/java/ru/kungfuept/randomteleport/commands/SafeBack.java
+++ src/main/java/ru/kungfuept/randomteleport/commands/SafeBack.java
@@ -1,22 +1,42 @@
 package ru.kungfuept.randomteleport.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
 
 public class SafeBack implements CommandExecutor {
 
+    public HashMap<String, Long> cooldowns = new HashMap<String, Long>();
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof ConsoleCommandSender) {
+            commandSender.getServer().getLogger().warning("[RandomTeleport] You must be a player!");
+            return true;
+        }
+
         Player player = (Player) commandSender;
         World world = player.getWorld();
         Location playerLoc = player.getLocation();
         Location spawn = world.getSpawnLocation();
+
+        int cooldownTime = 30;
+
+        if (cooldowns.containsKey(player.getName())) {
+            long secondsLeft = ((cooldowns.get(player.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
+            if (secondsLeft > 0) {
+                player.sendMessage(ChatColor.RED + "Вы не можете использовать эту команду ещё "+ ChatColor.YELLOW + secondsLeft + ChatColor.RED +" секунд!");
+                return true;
+            }
+        }
+
+        cooldowns.put(player.getName(), System.currentTimeMillis());
 
         double distanceLoc = Math.sqrt(Math.pow((playerLoc.getBlockX() - spawn.getBlockX()), 2) + Math.pow((playerLoc.getBlockZ() - spawn.getBlockZ()), 2));
         int distance = (int) Math.round(distanceLoc);
@@ -35,9 +55,15 @@ public class SafeBack implements CommandExecutor {
             int randomY = world.getHighestBlockYAt(randomX, randomZ);
             Location randomtpspawn = new Location(world, randomX, randomY, randomZ);
             player.teleport(randomtpspawn);
-            
+
+            if (!(player.getLocation().getBlock().getType() == Material.AIR)) {
+                Location loc = new Location(world, player.getLocation().getBlockX(), world.getHighestBlockYAt(player.getLocation()), player.getLocation().getBlockZ());
+                player.teleport(loc);
+            }
+
             player.sendMessage(ChatColor.GREEN + "Вы успешно телепортированы на спавн!");
-        } else {
+        }
+        else {
             player.sendMessage(ChatColor.DARK_RED + "Вы слишком далеко от спавна!");
         }
         return true;
--- src/main/java/ru/kungfuept/randomteleport/commands/SafeLeave.java
+++ src/main/java/ru/kungfuept/randomteleport/commands/SafeLeave.java
@@ -1,21 +1,42 @@
 package ru.kungfuept.randomteleport.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
 public class SafeLeave implements CommandExecutor {
+
+    public HashMap<String, Long> cooldowns = new java.util.HashMap<String, Long>();
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof ConsoleCommandSender) {
+            commandSender.getServer().getLogger().warning("[RandomTeleport] You must be a player!");
+            return true;
+        }
+
         Player player = (Player) commandSender;
         World world = player.getWorld();
         Location zero = player.getLocation().zero();
         Location playerLoc = player.getLocation();
+
+        int cooldownTime = 30;
+
+        if (cooldowns.containsKey(player.getName())) {
+            long secondsLeft = ((cooldowns.get(player.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
+            if (secondsLeft > 0) {
+                player.sendMessage(ChatColor.RED + "Вы не можете использовать эту команду ещё "+ ChatColor.YELLOW + secondsLeft + ChatColor.RED +" секунд!");
+                return true;
+            }
+        }
+
+        cooldowns.put(player.getName(), System.currentTimeMillis());
+
         double distanceLoc = Math.sqrt(Math.pow((playerLoc.getBlockX() - zero.getBlockX()), 2) + Math.pow((playerLoc.getBlockZ() - zero.getBlockZ()), 2));
         int distance = (int) Math.round(distanceLoc);
 
@@ -34,6 +55,12 @@ public class SafeLeave implements CommandExecutor {
 
             player.teleport(randomtp);
             player.sendMessage(ChatColor.GREEN + "Вы успешно рандомно телепортированы!");
+
+            if (!(player.getLocation().getBlock().getType() == Material.AIR)) {
+                Location loc = new Location(world, player.getLocation().getBlockX(), world.getHighestBlockYAt(player.getLocation()), player.getLocation().getBlockZ());
+                player.teleport(loc);
+            }
+
         } else {
             player.sendMessage(ChatColor.DARK_RED + "Вы и так далеко от спавна!");
         }
